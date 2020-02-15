92cc23ff24ee6dc6a540d9631e553ea527577875
Hisao Setoguchi <fire.crystal.in.myheart@gmail.com>
2020-02-15 18:16:16 +0900

    Create 154_B.cpp

---
+++ 154_B.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  string S;
+  
+  cin >> S;
+  
+  int n = S.size();
+
+  for (int i = 0; i < n; i++)
+    cout << "x";
+  cout << endl;
+  
+  return 0;
+}
