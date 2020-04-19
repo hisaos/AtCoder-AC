243f0c87c3f95c7e1fcb2de16b6a903be23f6570
Hisao Setoguchi <setoguchi@hisaos.com>
2020-04-19 22:45:29 +0900

    update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,3 +1,35 @@
+// [0, n-1]の和を返す
+int sum_from_zero(int n)
+{
+  int s = 0;
+  for (int i = 0; i < n; i ++)
+    s += i;
+  return s;
+}
+
+// nを1番目としてnから降順にr番目までの数の和を返す
+int rev_sum(int n, int r)
+{
+  int s = 0;
+  for (int i = n; i > n - r; -- i)
+    s += i;
+  return s;
+}
+
+// n!(階乗)
+int factorial(int n)
+{
+  if (n == 1) return 1;
+  else return n * factorial(n - 1);
+}
+
+// nCr(組み合わせ)
+int combination(int n, int r)
+{
+  return factorial(n) / (factorial(n - r) * factorial(r));
+}
+
+// a, bの最大公約数
 int gcd(int a, int b)
 {
   int r = a % b;
